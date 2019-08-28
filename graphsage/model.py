4daa3ebe2b02ffccc516d51284710df4d6d167cc
hoang dzung <dungmin97@gmail.com>
2019-08-28 16:42:18 +0700

    pretrained mincut graphsage

--- graphsage/model.py
+++ graphsage/model.py
@@ -11,11 +11,50 @@ from collections import defaultdict
 
 from graphsage.encoders import Encoder
 from graphsage.aggregators import MeanAggregator
+from graphsage.classify import classify
 
 """
 Simple supervised GraphSAGE model as well as examples running the model
 on the Cora and Pubmed datasets.
 """
+import torch 
+from torch.autograd import Variable
+import torch.nn.functional as F
+from torch import nn 
+from torch.nn import init
+
+import sys 
+
+TEMP = float(sys.argv[1])
+BETA = float(sys.argv[2])
+
+def sample_gumbel(shape, eps=1e-20):
+    U = torch.rand(shape)
+    if torch.cuda.is_available():
+        U = U.cuda()
+    return -Variable(torch.log(-torch.log(U + eps) + eps))
+
+def gumbel_softmax_sample(logits, temperature,beta=1.0):
+    y = logits + beta*sample_gumbel(logits.size())
+    return F.softmax(y / temperature, dim=-1)
+
+def gumbel_softmax(logits, temp, hard=False,beta=1.0):
+    """
+    input: [*, n_class]
+    return: [*, n_class] an one-hot vector
+    """
+    y = gumbel_softmax_sample(logits, temp,beta)
+    shape = y.size()
+    _, ind = y.max(dim=-1)
+    y_hard = torch.zeros_like(y).view(-1, shape[-1])
+    if torch.cuda.is_available():
+        y_hard = y_hard.cuda()
+    y_hard.scatter_(1, ind.view(-1, 1), 1)
+    y_hard = y_hard.view(*shape)
+    if hard:
+        return (y_hard - y).detach() + y
+    else:
+        return y
 
 class SupervisedGraphSage(nn.Module):
 
@@ -30,11 +69,21 @@ class SupervisedGraphSage(nn.Module):
     def forward(self, nodes):
         embeds = self.enc(nodes)
         scores = self.weight.mm(embeds)
-        return scores.t()
+        return embeds.t(), scores.t()
 
     def loss(self, nodes, labels):
-        scores = self.forward(nodes)
+        _, scores = self.forward(nodes)
         return self.xent(scores, labels.squeeze())
+
+    def nmincut_loss(self, nodes, adj, temp=1.0, hard=False, beta=1):
+        embeds, _ = self.forward(nodes)
+        assign_tensor = gumbel_softmax(embeds, temp, hard, beta)
+        super_adj = torch.transpose(assign_tensor, 0, 1) @ adj @ assign_tensor
+        vol = super_adj.sum(1)
+        diag = torch.diagonal(super_adj)
+        norm_cut = (vol - diag)/vol
+        lozz = norm_cut.sum()
+        return lozz
 
 def load_cora():
     num_nodes = 2708
@@ -53,6 +102,7 @@ def load_cora():
             labels[i] = label_map[info[-1]]
 
     adj_lists = defaultdict(set)
+    adj = np.zeros((len(node_map), len(node_map)))
     with open("cora/cora.cites") as fp:
         for i,line in enumerate(fp):
             info = line.strip().split()
@@ -60,15 +110,18 @@ def load_cora():
             paper2 = node_map[info[1]]
             adj_lists[paper1].add(paper2)
             adj_lists[paper2].add(paper1)
-    return feat_data, labels, adj_lists
+            adj[paper1][paper2]=1
+            adj[paper2][paper1]=1
+    return feat_data, labels, adj_lists, adj
 
 def run_cora():
     np.random.seed(1)
     random.seed(1)
     num_nodes = 2708
-    feat_data, labels, adj_lists = load_cora()
+    feat_data, labels, adj_lists, adj = load_cora()
     features = nn.Embedding(2708, 1433)
     features.weight = nn.Parameter(torch.FloatTensor(feat_data), requires_grad=False)
+    adj = Variable(torch.FloatTensor(adj), requires_grad=False)
    # features.cuda()
 
     agg1 = MeanAggregator(features, cuda=True)
@@ -81,12 +134,32 @@ def run_cora():
 
     graphsage = SupervisedGraphSage(7, enc2)
 #    graphsage.cuda()
-    rand_indices = np.random.permutation(num_nodes)
-    test = rand_indices[:1000]
-    val = rand_indices[1000:1500]
-    train = list(rand_indices[1500:])
+    # rand_indices = np.random.permutation(num_nodes)
+    # test = rand_indices[:1000]
+    # val = rand_indices[1000:1500]
+    # train = list(rand_indices[1500:])
+
+    train = list(range(140))
+    val = np.array(range(200, 500))
+    test = np.array(range(500, 1500))
 
     optimizer = torch.optim.SGD(filter(lambda p : p.requires_grad, graphsage.parameters()), lr=0.7)
+    temp = TEMP
+    for batch in range(10000):
+        batch_nodes = list(range(num_nodes))
+        start_time = time.time()
+        optimizer.zero_grad()
+        loss = graphsage.nmincut_loss(batch_nodes, adj,temp=TEMP, beta=BETA)
+        loss.backward()
+        optimizer.step()
+        end_time = time.time()
+        times.append(end_time-start_time)
+        if batch % 100 == 0:
+            accs = classify(graphsage.embeds.detach().cpu().numpy(),labels, 0.5)
+            print(batch, loss.item(), accs)
+        if batch % 1000:
+            temp = max(0.1, TEMP*np.exp(-0.0003*batch))
+
     times = []
     for batch in range(100):
         batch_nodes = train[:256]
@@ -99,7 +172,7 @@ def run_cora():
         optimizer.step()
         end_time = time.time()
         times.append(end_time-start_time)
-        print (batch, loss.data[0])
+        print (batch, loss.item())
 
     val_output = graphsage.forward(val) 
     print ("Validation F1:", f1_score(labels[val], val_output.data.numpy().argmax(axis=1), average="micro"))
@@ -153,14 +226,10 @@ def run_pubmed():
 
     graphsage = SupervisedGraphSage(3, enc2)
 #    graphsage.cuda()
-    # rand_indices = np.random.permutation(num_nodes)
-    # test = rand_indices[:1000]
-    # val = rand_indices[1000:1500]
-    # train = list(rand_indices[1500:])
-
-    train = list(range(140))
-    val = np.array(range(200, 500))
-    test = np.array(range(500, 1500))
+    rand_indices = np.random.permutation(num_nodes)
+    test = rand_indices[:1000]
+    val = rand_indices[1000:1500]
+    train = list(rand_indices[1500:])
 
     optimizer = torch.optim.SGD(filter(lambda p : p.requires_grad, graphsage.parameters()), lr=0.7)
     times = []
