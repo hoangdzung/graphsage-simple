3d73d45d077bfc734fac1b1e62c806ea05ef365b
hoang dzung <dungmin97@gmail.com>
2019-08-28 18:04:11 +0700

    add unsup graphsage

--- graphsage/model_unsup.py
+++ graphsage/model_unsup.py
@@ -26,8 +26,36 @@ from torch.nn import init
 
 import sys 
 
-TEMP = float(sys.argv[1])
-BETA = float(sys.argv[2])
+NUM_SAMPLED = int(sys.argv[1])
+
+def fixed_unigram_candidate_sampler(num_sampled, unique, range_max, distortion, unigrams):
+    weights = unigrams**distortion
+    prob = weights/weights.sum()
+    sampled = np.random.choice(range_max, num_sampled, p=prob, replace=~unique)
+    return sampled
+
+def sigmoid_cross_entropy_with_logits(labels, logits):
+    sig_aff = torch.sigmoid(logits)
+    loss = labels * -torch.log(sig_aff+1e-10) + (1 - labels) * -torch.log(1 - sig_aff+1e-10)
+    return loss
+
+def node2vec(outputs1, outputs2, neg_outputs, neg_sample_weights=1.0):
+    outputs1 = F.normalize(outputs1, dim=1)
+    outputs2 = F.normalize(outputs2, dim=1)
+    neg_outputs = F.normalize(neg_outputs, dim=1)
+
+    true_aff = F.cosine_similarity(outputs1, outputs2)
+    neg_aff = outputs1.mm(neg_outputs.t())    
+    true_labels = torch.ones(true_aff.shape)
+    if torch.cuda.is_available():
+        true_labels = true_labels.cuda()
+        true_xent = sigmoid_cross_entropy_with_logits(labels=true_labels, logits=true_aff)
+    neg_labels = torch.zeros(neg_aff.shape)
+    if torch.cuda.is_available():
+        neg_labels = neg_labels.cuda()
+    neg_xent = sigmoid_cross_entropy_with_logits(labels=neg_labels, logits=neg_aff)
+    loss = true_xent.sum() + neg_sample_weights * neg_xent.sum()
+    return loss  
 
 class SupervisedGraphSage(nn.Module):
 
@@ -49,15 +77,12 @@ class SupervisedGraphSage(nn.Module):
         _, scores = self.forward(nodes)
         return self.xent(scores, labels.squeeze())
 
-    def nmincut_loss(self, nodes, adj, temp=1.0, hard=False, beta=1):
-        embeds, _ = self.forward(nodes)
-        assign_tensor = gumbel_softmax(embeds, temp, hard, beta)
-        super_adj = torch.transpose(assign_tensor, 0, 1) @ adj @ assign_tensor
-        vol = super_adj.sum(1)
-        diag = torch.diagonal(super_adj)
-        norm_cut = (vol - diag)/vol
-        lozz = norm_cut.sum()
-        return lozz
+    def unsup_loss(self, nodes1, nodes2, neg_nodes):
+        embeds1, _ = self.forward(nodes1)
+        embeds2, _ = self.forward(nodes2)
+        neg_embeds, _ = self.forward(neg_nodes)
+        loss = node2vec(embeds1, embeds2, neg_embeds)
+        return loss 
 
 def load_cora():
     num_nodes = 2708
@@ -77,6 +102,7 @@ def load_cora():
 
     adj_lists = defaultdict(set)
     adj = np.zeros((len(node_map), len(node_map)))
+    edges = []
     with open("cora/cora.cites") as fp:
         for i,line in enumerate(fp):
             info = line.strip().split()
@@ -86,13 +112,14 @@ def load_cora():
             adj_lists[paper2].add(paper1)
             adj[paper1][paper2]=1
             adj[paper2][paper1]=1
-    return feat_data, labels, adj_lists, adj
+            edges.append([paper1, paper2])
+    return feat_data, labels, adj_lists, adj, adj.sum(1), np.array(edges)
 
 def run_cora():
     np.random.seed(1)
     random.seed(1)
     num_nodes = 2708
-    feat_data, labels, adj_lists, adj = load_cora()
+    feat_data, labels, adj_lists, adj, degrees, edges = load_cora()
     features = nn.Embedding(2708, 1433)
     features.weight = nn.Parameter(torch.FloatTensor(feat_data), requires_grad=False)
     adj = Variable(torch.FloatTensor(adj), requires_grad=False)
@@ -121,18 +148,30 @@ def run_cora():
     test = np.array(range(500, 1500))
 
     optimizer = torch.optim.SGD(filter(lambda p : p.requires_grad, graphsage.parameters()), lr=0.7)
-    temp = TEMP
-    for batch in range(10000):
-        batch_nodes = list(range(num_nodes))
+
+    times = []
+    for batch in range(100):
+        batch_edges = edges[:256]
+        random.shuffle(batch_edges)
+        nodes1, nodes2 = batch_edges[:, 0], batch_edges[:,1]
+        neg_nodes = fixed_unigram_candidate_sampler(
+            num_sampled=NUM_SAMPLED,
+            unique=False,
+            range_max=len(degrees),
+            distortion=0.75,
+            unigrams=degrees
+        )   
+        start_time = time.time()
         optimizer.zero_grad()
-        loss = graphsage.nmincut_loss(batch_nodes, adj,temp=temp, beta=BETA)
+        embeds1,_ = graphsage.forward(nodes1)
+        embeds2,_ = graphsage.forward(nodes2)
+        neg_embeds,_ = graphsage.forward(neg_nodes)
+        loss = graphsage.unsup_loss(embeds1, embeds2, neg_embeds)
         loss.backward()
         optimizer.step()
-        if batch % 100 == 0:
-            accs = classify(graphsage.embeds.detach().cpu().numpy(),labels, 0.5)
-            print(batch, loss.item(), accs)
-        if batch % 1000:
-            temp = max(0.1, TEMP*np.exp(-0.0003*batch))
+        end_time = time.time()
+        times.append(end_time-start_time)
+        print (batch, loss.item())
 
     times = []
     for batch in range(100):
@@ -151,7 +190,7 @@ def run_cora():
         print (batch, loss.item())
 
     _,test_output = graphsage.forward(test) 
-    print ("Testing F1:", f1_score(labels[test], test_output.data.numpy().argmax(axis=1), average="micro"))
+    print ("Testing F1:", f1_score(labels[test], test_output.detach().cpu().numpy().argmax(axis=1), average="micro"))
     print ("Average batch time:", np.mean(times))
 
 def load_pubmed():
